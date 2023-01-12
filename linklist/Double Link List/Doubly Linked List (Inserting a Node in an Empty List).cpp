e2ac84bbd1ce39fac2cccca63684db91c1d2ffc6
Ajoy Nath <90346280+AjoyDevNath@users.noreply.github.com>
2023-01-12 23:06:45 +0600

    Add files via upload

---
+++ linklist/Double Link List/Doubly Linked List (Inserting a Node in an Empty List).cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *previous;
+    Node *next;
+};
+
+Node *adding_node(Node *head,int data)
+{
+    Node *temp = NULL;
+    temp = new Node();
+    temp -> previous = NULL;
+    temp -> data = 45;
+    temp -> next = NULL;
+
+    head = temp;
+    return head;
+}
+
+int main()
+{
+    Node* head = NULL;
+    head = adding_node(head,45);
+    cout<<head -> data<<"\n";
+    return 0;
+}
